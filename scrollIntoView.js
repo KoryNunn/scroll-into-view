fab21e106b84f54a9f5eb8e7258883d4e8cd31f0
KoryNunn <knunn187@gmail.com>
2014-03-27 16:46:24 +1000

    trying to fix implementation

--- scrollIntoView.js
+++ scrollIntoView.js
@@ -4,10 +4,10 @@ module.exports = function(target){
     }
 
     var parent = target.parentNode,
-        targetPosition;
+        targetPosition = target.getBoundingClientRect(),
+        parentOverflow;
 
-    while(parent && parent !== document){
-        targetPosition = target.getBoundingClientRect();
+    while(parent && parent.tagName !== 'BODY'){
         parentOverflow = window.getComputedStyle(parent).overflow;
         if(
             parentOverflow !== 'auto' ||
@@ -15,10 +15,15 @@ module.exports = function(target){
             parentOverflow !== 'scrollX' ||
             parentOverflow !== 'scrollY'
         ){
-            parent.scrollTop += targetPosition.top - parent.clientHeight / 2;
-            parent.scrollLeft += targetPosition.left - parent.clientWidth / 2;
+            parent.scrollTop = target.offsetTop + Math.min(targetPosition.height, parent.clientHeight) / 2;
+            parent.scrollLeft = target.offsetTop + Math.min(targetPosition.width, parent.clientWidth) / 2;
         }
 
         parent = parent.parentNode;
     }
+
+    window.scrollTo(
+        targetPosition.left + window.scrollX - window.innerWidth / 2 + Math.min(targetPosition.width, window.innerWidth) / 2,
+        targetPosition.top + window.scrollY - window.innerHeight / 2 + Math.min(targetPosition.height, window.innerHeight) / 2
+    );
 };
