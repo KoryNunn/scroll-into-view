78d8c39f926b02bb0603d302bd3dce0c75efc131
KoryNunn <knunn187@gmail.com>
2014-03-28 11:28:28 +1000

    Removed debug, improved scrolling finctionality

--- scrollIntoView.js
+++ scrollIntoView.js
@@ -1,26 +1,67 @@
-function transitionScrollTo(element, x, y, startTime){
+function setElementScroll(element, x, y){
+    if(element === window){
+        element.scrollTo(x, y);
+    }else{
+
+        element.scrollLeft = x;
+        element.scrollTop = y;
+    }
+}
+
+function getTargetScrollLocation(target, parent){
+    var targetPosition = target.getBoundingClientRect(),
+        x,
+        y,
+        differenceX,
+        differenceY;
+
+    if(parent === window){
+        x = targetPosition.left + window.scrollX - window.innerWidth / 2 + Math.min(targetPosition.width, window.innerWidth) / 2;
+        y = targetPosition.top + window.scrollY - window.innerHeight / 2 + Math.min(targetPosition.height, window.innerHeight) / 2;
+        x = Math.max(Math.min(x, document.body.clientWidth - window.innerWidth), 0);
+        y = Math.max(Math.min(y, document.body.clientHeight - window.innerHeight), 0);
+        differenceX = x - window.scrollX;
+        differenceY = y - window.scrollY;
+    }else{
+        x = Math.min(target.offsetLeft + (targetPosition.width / 2) - parent.clientWidth / 2, target.offsetLeft);
+        y = Math.min(target.offsetTop + (targetPosition.height / 2) - parent.clientHeight / 2, target.offsetTop);
+        x = Math.max(Math.min(x, parent.scrollWidth - parent.clientWidth), 0);
+        y = Math.max(Math.min(y, parent.scrollHeight - parent.clientHeight), 0);
+        differenceX = x - parent.scrollLeft;
+        differenceY = y - parent.scrollTop;
+    }
+
+    return {
+        x: x,
+        y: y,
+        differenceX: differenceX,
+        differenceY: differenceY
+    };
+}
+
+function transitionScrollTo(target, parent, startTime){
     if(!startTime){
         startTime = +new Date();
     }
 
-    x = Math.max(Math.min(x, element.scrollWidth - element.clientWidth), 0);
-    y = Math.max(Math.min(y, element.scrollHeight - element.clientHeight), 0);
-
-    var differenceY = y - element.scrollTop,
-        differenceX = x - element.scrollLeft;
-
     requestAnimationFrame(function(){
+        var location = getTargetScrollLocation(target, parent);
+
         if(new Date() - startTime > 350){
             // Give up and set the scroll position
-            element.scrollTop += differenceY;
-            element.scrollLeft += differenceX;
+            setElementScroll(parent,
+                location.x,
+                location.y
+            );
             return;
         }
-        element.scrollTop += differenceY * 0.2 + 1;
-        element.scrollLeft += differenceX * 0.2 + 1;
+        setElementScroll(parent,
+            location.x - location.differenceX * 0.3,
+            location.y - location.differenceY * 0.3
+        );
 
-        if(Math.abs(differenceY) > 1 || Math.abs(differenceX) > 1){
-            transitionScrollTo(element, x, y, startTime);
+        if(Math.abs(location.differenceY) > 1 || Math.abs(location.differenceX) > 1){
+            transitionScrollTo(target, parent, startTime);
         }
     });
 }
@@ -42,18 +83,11 @@ module.exports = function(target){
             parentOverflow === 'scrollX' ||
             parentOverflow === 'scrollY'
         ){
-            transitionScrollTo(parent,
-                Math.min(target.offsetLeft + (targetPosition.width / 2) - parent.clientWidth / 2, target.offsetLeft),
-                Math.min(target.offsetTop + (targetPosition.height / 2) - parent.clientHeight / 2, target.offsetTop)
-            )
+            transitionScrollTo(target, parent);
         }
-
 
         parent = parent.parentNode;
     }
 
-    transitionScrollTo(parent,
-        targetPosition.left + window.scrollX - window.innerWidth / 2 + Math.min(targetPosition.width, window.innerWidth) / 2,
-        targetPosition.top + window.scrollY - window.innerHeight / 2 + Math.min(targetPosition.height, window.innerHeight) / 2
-    );
+    transitionScrollTo(target, window);
 };
